b034d34f0622dbc65a137c507d728ea08a3bfe8f
G00401655@atu.ie <G00401655@atu.ie>
2024-10-21 16:01:21 +0100

    Setting Up for Lab Exam

---
+++ src/main/java/ie/atu/labexamra/LabExamRaApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.labexamra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamRaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamRaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/labexamra/LabExamRaApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.labexamra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamRaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
